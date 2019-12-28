9d58e91a92c3451a3a6054096515eddb2648f621
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2019-12-28 14:10:24 +0100

    Improve word highlighting inverse warp

--- koda/engine.py
+++ koda/engine.py
@@ -1,6 +1,7 @@
 from .corners.detector import CornersDetectorByEdges, CornersNotFound
 from .ocr.tesseract import TesseractOCREngine
 from .utils import *
+from .document import *
 import cv2
 
 class DocumentNotFound(CornersNotFound):
@@ -9,48 +10,6 @@ class DocumentNotFound(CornersNotFound):
     """
     def __init__(self, message):
         super().__init__(message)
-
-class Document:
-    """
-    Model class for a detected document
-
-    Attributes:
-        img: The warped image of the document
-        words: List of all single word found in the document by OCR
-
-    Methods:
-        findWord: Search for a specified word in the document
-    """
-    def __init__(self, words, img, warped, M):
-        self.words = words
-        self.img = img
-        self.warped = warped
-        _, self.IM = cv2.invert(M)
-
-    def findWord(self, word):
-        """
-        Find a word within the document
-
-        :returns: A tuple of two images copy with the searched word highlighted. The first is a copy of the original image, the second a copy of the warped one.
-        """
-        overlay, overlay_warped = self.img.copy(), self.warped.copy()
-        color = (0, 255, 255)
-        alpha = 0.3
-        for w in self.words:
-            if (word.lower() in w[0].lower()):
-                # Warped
-                tl, br = (w[1], w[2]), (w[3], w[4])
-                cv2.rectangle(overlay_warped, tl, br, color, cv2.FILLED)
-                res_warped = cv2.addWeighted(overlay_warped, alpha, self.warped, 1-alpha, 0)
-
-                # Original
-                r = np.array([[[w[1], w[2]], [w[3], w[4]]]], dtype=np.float32)
-                r = cv2.perspectiveTransform(r, self.IM)[0]
-                tl, br = (r[0][0], r[0][1]), (r[1][0], r[1][1])
-                cv2.rectangle(overlay, tl, br, color, cv2.FILLED)
-                res = cv2.addWeighted(overlay, alpha, self.img, 1-alpha, 0)
-
-        return (res, res_warped)
 
 class DetectionEngine:
     """
